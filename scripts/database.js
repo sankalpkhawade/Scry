5b72b2612dea7cdb8b3251ae94ba89578e339b3e
sankalp <sankalpkhawade1997@gmail.com>
2019-10-16 23:22:09 +0530

    Update database.js

--- scripts/database.js
+++ scripts/database.js
@@ -2,6 +2,7 @@ const mongoose = require('../utilities/mongoose.js');
 
 // This was to initialize MongoDB in a separate process in order to facilitate
 // proper database functionality.
+//prooer shutdown functionality.
 // This appears to be deprecated, as proper shutdown timing avoids this need.
 const main = () => {
   mongoose.initialize();
